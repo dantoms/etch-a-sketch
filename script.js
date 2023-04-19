470c0a96f8e78762766c6ad40da0867c60259461
0garrow <0garrow@protonmail.com>
2023-04-19 13:50:16 +0100

    Implement grid

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 const squares = 4;
 const gridContainer = document.getElementById('grid-container');
-gridContainer.innerText = 'grid-container';
+gridContainer.style.width = `${squares * 20}px`;
+
+// counter for
+for (let index = 0; index < squares; index++) {
+  const row = document.createElement('div');
+  row.classList.add('row');
+  for (let index = 0; index < squares; index++) {
+    const box = document.createElement('div');
+    box.classList.add('box');
+    row.appendChild(box);
+  }
+  gridContainer.appendChild(row);
+}
